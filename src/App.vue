bbc2c997082a39e65227038f9c28aa94105358c7
MCigno94 <mattiacigno27@gmail.com>
2022-05-05 15:30:27 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+
+    <SiteHeader />
+    
+    <main>
+
+    </main>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import SiteHeader from './components/HeaderComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SiteHeader,
+  },
+  data() {
+    return {
+      API_URL: 'https://flynn.boolean.careers/exercises/api/array/music',
+      discs: null,
+      loading: true,
+      error: null
+    }
+  },
+  methods: {},
+  mounted(){
+    axios
+    .get(this.API_URL)
+    .then(response => {
+        console.log(response);
+        this.discs = response.data;
+        this.loading = false;
+    })
+    .catch(error => {
+        console.log(error);
+        this.error = `Sorry There is a problem! ${error}`
+    });
+  },
+}
+</script>
+
+<style lang="scss">
+@use '@/assets/scss/style.scss';
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="@/assets/img/Spotify.png" alt="">
+    </div>
+    <!-- /.logo -->
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
